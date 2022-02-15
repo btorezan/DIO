4ee956af3e3b4894b131ba26c02ab66da44d8a9e
btorezan <bruno.torezan@gmail.com>
2022-02-15 16:37:16 -0300

    Finalização do Projeto Clinica

--- Clinica/src/br/com/torezan/clinica/Medico.java
+++ Clinica/src/br/com/torezan/clinica/Medico.java
@@ -1,4 +1,39 @@
 package br.com.torezan.clinica;
 
 public class Medico {
+    private final String nome;
+    private int id;
+    private final String crm;
+    private Agenda agenda;
+    private boolean emAtendimento = false;
+
+    public Medico(String nome, int id, String crm) {
+        this.nome = nome;
+        this.id = id;
+        this.crm = crm;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCrm() {
+        return crm;
+    }
+
+    public void setAgenda(Agenda agenda){
+        this.agenda = agenda;
+    }
+
+    public Agenda getAgenda(){
+        return this.agenda;
+    }
+
+    public boolean isEmAtendimento() {
+        return emAtendimento;
+    }
+
+    public void setEmAtendimento(boolean emAtendimento) {
+        this.emAtendimento = emAtendimento;
+    }
 }
